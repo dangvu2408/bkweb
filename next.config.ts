93b924d00edcc9c9e67b28dcb1ae5e0c6bf19a64
dangvu2408 <hiend6887@gmail.com>
2025-07-25 23:49:27 +0700

    pre commit (25072025)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false, // This is the options that hide/remove the icon
 };
 
 export default nextConfig;
