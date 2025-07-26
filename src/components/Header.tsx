dfd313bba47ada59aaab1c8d56aaba0fa243080a
dangvu2408 <hiend6887@gmail.com>
2025-07-26 23:51:26 +0700

    1st commit (27072025) - search container
    + create header component (Header.tsx), sidebar component (Sidebar.tsx) and footer component (Footer.tsx)
    + using Tailwind to styling the search container in header component.

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+export default function Header() {
+    return (
+        <header className="flex items-center fixed top-0 right-0 h-[70px] px-[60px] z-[100] min-w-[660px] left-[240px]">
+            <div className="flex items-center justify-between relative z-[1] w-full">
+                <div className="flex items-center justify-start flex-grow mr-[10px] basis-auto shrink-0">
+                    <form className="relative w-full max-w-[440px]">
+                        <div className="flex items-center gap-[10px] px-[10px] h-[40px] rounded-[20px] bg-[rgba(0,0,0,0.05)]">
+                            <button className="inline-flex items-center text-[14px] rounded-full leading-normal border-0 font-normal uppercase-none text-center cursor-pointer relative">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#696969" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search cursor-pointer text-[20px] left-[10px]">
+                                    <circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                                </svg>
+                            </button>
+                            <div className="w-full">
+                                <input type="text" placeholder="Tìm kiếm tiện ích..." className="m-0 inline-block w-[95%] text-[14px] text-[#282828] bg-clip-padding border-0 relative h-[34px] px-0 py-[5px] leading-[34px] focus:outline-none focus:ring-0 focus:border-none"></input>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div className="flex items-center justify-end flex-grow mr-[10px] basis-auto shrink-0">
+
+                </div>
+            </div>
+        </header>
+    );
+}
